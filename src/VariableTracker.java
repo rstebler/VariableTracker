e807381eee8560fab7f6ba89bebc5a346f0b6420
Roger Stebler <roger.stebler@students.unibe.ch>
2014-05-30 18:32:49 +0200

    Adds some result informations

--- src/VariableTracker.java
+++ src/VariableTracker.java
@@ -15,6 +15,8 @@ import java.util.List;
 public class VariableTracker {
 
 	public static void main(String[] args) {
+		int totalVariables = 0;
+		int totalVariablesWithDifferentTypes = 0;
 		MongoClient mongoClient = null;
 		try {
 			mongoClient = new MongoClient("localhost");
@@ -25,45 +27,54 @@ public class VariableTracker {
 		if (mongoClient != null) {
 			DB db = mongoClient.getDB("test");
 			DBCollection coll = db.getCollection("variableInformation");
+			long totalAssignments = coll.count();
+			
+			System.out.println("Total entries in the database: " + totalAssignments);
 
 			List<String> classNames = coll.distinct("variableClass");
 
 			for (String className : classNames) {
 				// Go through all class names
-				//System.out.println(className);
 				BasicDBObject methodQuery = new BasicDBObject("variableClass", className);
 				List<String> methodNames = coll.distinct("variableMethod", methodQuery);
 
 				for (String methodName : methodNames) {
 					// Go through all method names
-					//System.out.println("   "+methodName);
 					BasicDBObject variableNameQuery = new BasicDBObject("variableClass", className).append("variableMethod", methodName);
 					List<String> variableNames = coll.distinct("variableName", variableNameQuery);
 
 					for (String variableName : variableNames) {
 						// Go through all variable names
-						//System.out.println("     "+variableName);
 						BasicDBObject variableTypeQuery = new BasicDBObject("variableClass", className).append("variableMethod", methodName).append("variableName", variableName);
 						List<String> variableTypes = coll.distinct("variableType", variableTypeQuery);
 
 						if(variableTypes.size() > 1) {
 							// This variable has multiple types!
-							System.out.println("!!! Multiple variable types in "+ className + ">>" + methodName +">>"+variableName+" !!!");
+							totalVariablesWithDifferentTypes++;
+							System.out.println("Multiple variable types found for "+ className + ">>" + methodName +">>"+variableName);
 						}
+						totalVariables++;
 						for (String variableType : variableTypes) {
 							// Go through all types names
 							// Count how many times the variable has this type
 							BasicDBObject variableTypeCountQuery = new BasicDBObject("variableClass", className).append("variableMethod", methodName).append("variableName", variableName).append("variableType", variableType);
 							long variableTypesCount = coll.count(variableTypeCountQuery);
 							
-							if( variableTypes.size() > 1) {
-								System.out.println("      "+variableType+" : "+ variableTypesCount+"x");
+							if(variableTypes.size() > 1) {
+								System.out.println("      " + variableType + " : " + variableTypesCount + "x");
 							}
 						}
 					}
 				}
-				//System.out.println();
 			}
+			
+			System.out.println();
+			System.out.println("Results");
+			System.out.println("-------");
+			System.out.println("Total variable assignments: " + totalAssignments);
+			System.out.println("Total variables: " + totalVariables);
+			System.out.println("--> every variable was assigned " + (float)Math.round((float)totalAssignments/totalVariables*100)/100  + " times");
+			System.out.println("Total variables with different types: " + totalVariablesWithDifferentTypes + " (" + (float)Math.round((float)totalVariablesWithDifferentTypes/totalVariables*10000)/100 + "%)");
 		}
 	}
 
